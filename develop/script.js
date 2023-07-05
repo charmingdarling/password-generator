42f74955bbd03fce8824465ea12e87a3b7308f61
Charming Darling <h1hasinai_f06@yahoo.com>
2023-07-04 22:29:56 -0700

    ReadMe file updated.

--- develop/script.js
+++ develop/script.js
@@ -4,51 +4,44 @@
 var generateBtn = document.querySelector("#generate");
 
 // Declare variables
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var number = "0123456789";
-var special = "!@#$%^&*+_-~";
-
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var number = ["0123456789"];
+var special = ["!#$()*+,-./:;<=>?@[^_`{|}~"];
 var character = "";
 var password = "";
-
 
 function generatePassword() {
   console.log("You clicked the button!");
 
-//Prompt the user for password criteria
-var characterLength = prompt("Please choose the number of character you want between 8 and 128."); 
+    //Prompt the user for password criteria
+    var characterLength = prompt("Please choose the number of character you want between 8 and 128."); 
 
-if (characterLength = NaN || characterLength < 8 || characterLength > 128) {
-  alert("Enter a valid number of characters");
-  return "Try again";
-  }
+    var length = Number(characterLength);
+    
+    if (length = NaN || characterLength < 8 || characterLength > 128) {
+      alert("Enter a valid number of characters");
+      return "Try again.";
+      }
 
-if (confirm)
-  var lowercase = (confirm("Would you like to use lowercase letters?"))
-  character = character.concat(lowercase);
+    if (confirm("Would you like to use lowercase letters?")){
+    character = character.concat(lowercase);}
 
+    if (confirm("Would you like to use uppercase letters?")){
+    character = character.concat(uppercase);}
 
-if (confirm)
-  var uppercase = (confirm("Would you like to use uppercase letters?")) 
-  character = character.concat(uppercase);
+    if (confirm("Would you like to use numbers?")){
+    character = character.concat(number)}
 
-if (confirm)
-  var number = (confirm("Would you like to use numbers?")) 
-  character = character.concat(number);
+    if (confirm("Would you like to use special characters?")){
+    character = character.concat(special);}
 
-
-if (confirm)
-  var special = (confirm("Would you like to use special characters?")) 
-  character = character.concat(special);
-
-
-for (var i = 0 ; i < characterLength; i++) {
-  password += character[Math.floor(Math.random()*characterLength.length)];
+    for (var i = 0 ; i < characterLength; i++) {
+    password += character[Math.floor(Math.random()*characterLength.length)];
+    }
+    
+    return password;
 }
-
-return password;
-};
 
 // Write password to the #password input
 function writePassword() {
@@ -60,16 +53,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-  //1b. lowercase, uppercase, numbers, special characters
-  //2. Validate the input
-  //3. Generate password based on criteria
-
-  //4. Display password on page
-  //return "Generated password will go here.";
-
-
-
-
-
-
